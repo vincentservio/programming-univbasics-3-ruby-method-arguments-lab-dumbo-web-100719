8e02d54d3a9f1861185261cde120959f6fd25a8c
vincent servio <vincentservio@gmail.com>
2019-08-30 01:34:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,9 +12,9 @@ def introduction_with_language (name, language)
   end
 
 def introduction_with_language_optional (name,language)
- puts "Hi, my name is #{name} and I am learning to program in Python."
- puts  "Hi, my name is #{name} and I am learning to program in #{language}"
-
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}"
+else 
+   puts  "Hi, my name is #{name} and I am learning to program in Ruby"
 end 
 
 
