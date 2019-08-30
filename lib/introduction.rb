0c41a7108ffb1c3c3c05d0f74ab6016c10754a35
vincent servio <vincentservio@gmail.com>
2019-08-30 01:41:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language (name, language)
 
 def introduction_with_language_optional (name,language)
   puts  "Hi, my name is #{name} and I am learning to program in #{language}"
-elsif
+
    puts  "Hi, my name is #{name} and I am learning to program in Ruby"
 end 
 end
